fa6abfe0c1808608740bdb08cfc040fe3c17ccbf
kunita <kunita@waltsinc.jp>
2019-08-16 18:50:54 +0900

    update

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ * クレカ情報を保存する
+ */
 
 require_once('../vendor/autoload.php');
 
@@ -8,46 +11,97 @@ $dotenv->load();
 $secretKey = getenv('STRIPE_SECRET_KEY');
 $publicKey = getenv('STRIPE_PUBLIC_KEY');
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey($secretKey);
 
-$session = \Stripe\Checkout\Session::create([
-    'payment_method_types' => ['card'],
-    'line_items' => [[
-        'name' => 'T-shirt',
-        'description' => 'Comfortable cotton t-shirt',
-        'images' => ['https://example.com/t-shirt.png'],
-        'amount' => 500,
-        'currency' => 'jpy',
-        'quantity' => 1,
-    ]],
-    'success_url' => 'https://example.com/success',
-    'cancel_url' => 'https://example.com/cancel',
+// Create a Customer:
+$customer = \Stripe\Customer::create([
+    'source' => 'tok_mastercard',
+    'email' => 'vartkg+1@gmail.com',
 ]);
+
+// Charge the Customer instead of the card:
+$charge = \Stripe\Charge::create([
+    'amount' => 1000,
+    'currency' => 'usd',
+    'customer' => $customer->id,
+]);
+
+// YOUR CODE: Save the customer ID and other info in a database for later.
+
+// When it's time to charge the customer again, retrieve the customer ID.
+//$charge = \Stripe\Charge::create([
+//    'amount' => 1500, // $15.00 this time
+//    'currency' => 'usd',
+//    'customer' => $customer_id, // Previously stored, then retrieved
+//]);
 
 ?>
 
 <script src="https://js.stripe.com/v3/"></script>
 
+<form action="/charge" method="post" id="payment-form">
+    <div class="form-row">
+        <label for="card-element">
+            Credit or debit card
+        </label>
+        <div id="card-element">
+            <!-- A Stripe Element will be inserted here. -->
+        </div>
+
+        <!-- Used to display Element errors. -->
+        <div id="card-errors" role="alert"></div>
+    </div>
+
+    <button>Submit Payment</button>
+</form>
+
 <script>
     const publicKey = '<?= $publicKey ?>';
 
     var stripe = Stripe(publicKey);
+    var elements = stripe.elements();
 
-    function onClick() {
-        console.log('aaa');
-        stripe.redirectToCheckout({
-            // Make the id field from the Checkout Session creation API response
-            // available to this file, so you can provide it as parameter here
-            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-            sessionId: '<?= $session->id ?>'
-        }).then(function (result) {
-            // If `redirectToCheckout` fails due to a browser or network
-            // error, display the localized error message to your customer
-            // using `result.error.message`.
+    // Custom styling can be passed to options when creating an Element.
+    var style = {
+        base: {
+            // Add your base input styles here. For example:
+            fontSize: '16px',
+            color: "#32325d",
+        }
+    };
+
+    // Create an instance of the card Element.
+    var card = elements.create('card', {style: style});
+
+    // Add an instance of the card Element into the `card-element` <div>.
+    card.mount('#card-element');
+
+    // 入力変更時のリスナー
+    // バリデーションメッセージを表示する
+    card.addEventListener('change', function(event) {
+        var displayError = document.getElementById('card-errors');
+        if (event.error) {
+            displayError.textContent = event.error.message;
+        } else {
+            displayError.textContent = '';
+        }
+    });
+
+    // submit時のリスナー
+    // stripeサーバでトークンに変換してからアプリのサーバにポストする
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        stripe.createToken(card).then(function(result) {
+            if (result.error) {
+                // Inform the customer that there was an error.
+                var errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+            } else {
+                // Send the token to your server.
+                stripeTokenHandler(result.token);
+            }
         });
-    }
+    });
 </script>
-
-<button onclick="onClick()">決済</button>
