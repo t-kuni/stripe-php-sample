d9381bbd7d46eb4df604c8a8556d4b687fda4c11
kunita <kunita@waltsinc.jp>
2019-08-28 18:10:25 +0900

    update

--- src/charge/index.php
+++ src/charge/index.php
@@ -71,26 +71,21 @@ $publicKey = getenv('STRIPE_PUBLIC_KEY');
 
         stripe.createToken(card).then(function(result) {
             if (result.error) {
-                // Inform the customer that there was an error.
                 var errorElement = document.getElementById('card-errors');
                 errorElement.textContent = result.error.message;
             } else {
-                // Send the token to your server.
                 stripeTokenHandler(result.token);
             }
         });
     });
 
     function stripeTokenHandler(token) {
-        // Insert the token ID into the form so it gets submitted to the server
         var form = document.getElementById('payment-form');
         var hiddenInput = document.createElement('input');
         hiddenInput.setAttribute('type', 'hidden');
         hiddenInput.setAttribute('name', 'stripeToken');
         hiddenInput.setAttribute('value', token.id);
         form.appendChild(hiddenInput);
-
-        // Submit the form
         form.submit();
     }
 </script>
